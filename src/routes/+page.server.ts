510e386982d928ad3d2df764dc7b07173b845296
Jonathan Gamble <jdgamble555@gmail.com>
2023-07-13 19:53:43 -0500

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -11,19 +11,12 @@ export const config = {
 
 export const actions = {
     default: async ({ fetch }) => {
-        const path = 'https://isr-test-ten.vercel.app';
-        const revalidateResponse = await fetch(path, {
+        await fetch('https://isr-test-ten.vercel.app/', {
             method: 'GET',
             headers: {
                 'x-prerender-revalidate': BYPASS_TOKEN
             }
         });
-        const cacheHeader = revalidateResponse.headers.get('x-vercel-cache');
-        if (cacheHeader !== 'REVALIDATED') {
-            console.error(new Error(`Revalidation of ${path} failed.`));
-            fail(404, { problem: `Revalidation of ${path} failed: "x-vercel-cache" is "${cacheHeader}"` });
-            return
-        }
     }
 } satisfies Actions;
 
