1450e27f1068695b9528a304d4b14b9d7f88f8c7
Jonathan Gamble <jdgamble555@gmail.com>
2023-07-13 19:46:43 -0500

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
 import type { Actions, PageServerLoad } from './$types';
 import { BYPASS_TOKEN } from '$env/static/private';
+import { fail } from '@sveltejs/kit';
 
 export const config = {
     isr: {
@@ -10,11 +11,19 @@ export const config = {
 
 export const actions = {
     default: async ({ fetch }) => {
-        fetch('https://isr-test-ten.vercel.app', {
+        const path = 'https://isr-test-ten.vercel.app';
+        const revalidateResponse = await fetch(path, {
+            method: 'GET',
             headers: {
                 'x-prerender-revalidate': BYPASS_TOKEN
             }
         });
+        const cacheHeader = revalidateResponse.headers.get('x-vercel-cache');
+        if (cacheHeader !== 'REVALIDATED') {
+            console.error(new Error(`Revalidation of ${path} failed.`));
+            fail(404, { problem: `Revalidation of ${path} failed: "x-vercel-cache" is "${cacheHeader}"` });
+            return
+        }
     }
 } satisfies Actions;
 
