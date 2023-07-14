d92a11eea82dc21c426aaf179fe4058f285d4b09
Jonathan Gamble <jdgamble555@gmail.com>
2023-07-13 20:25:13 -0500

    test

---
+++ src/routes/test/+page.server.ts
@@ -0,0 +1,29 @@
+import type { Actions, PageServerLoad } from './$types';
+import { BYPASS_TOKEN } from '$env/static/private';
+
+export const config = {
+    isr: {
+        expiration: false,
+        bypassToken: BYPASS_TOKEN
+    }
+};
+
+export const actions = {
+    default: async () => {
+        await fetch('https://isr-test-ten.vercel.app/test', {
+            method: 'GET',
+            headers: {
+                'x-prerender-revalidate': BYPASS_TOKEN
+            }
+        });
+    }
+} satisfies Actions;
+
+export const load = (async () => {
+
+    const n = Math.random();
+
+    return {
+        n
+    };
+}) satisfies PageServerLoad;
