7a695df3dcfbcfaa140f661bc16cac63ef838b2b
Jonathan Gamble <jdgamble555@gmail.com>
2023-07-13 19:55:43 -0500

    test

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -10,7 +10,7 @@ export const config = {
 
 export const actions = {
     default: async ({ fetch }) => {
-        await fetch('https://isr-test-ten.vercel.app/', {
+        await fetch('https://isr-test-ten.vercel.app/test', {
             method: 'GET',
             headers: {
                 'x-prerender-revalidate': BYPASS_TOKEN
